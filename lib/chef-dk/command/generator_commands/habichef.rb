d27344cfdb091548054fc81ef35abeb5e23dee7e
qubitrenegade <qubitrenegade@gmail.com>
2019-05-11 13:51:04 -0600

    first pass at adding modue via file and requiring it.

---
+++ lib/chef-dk/command/generator_commands/habichef.rb
@@ -0,0 +1,13 @@
+require 'chef-dk/command/generator_commands/base'
+
+module ChefDK
+  module Command
+    module GeneratorCommands
+      class HabiChef < Base
+        def run
+          puts %(I'm running !!!)
+        end
+      end
+    end
+  end
+end
